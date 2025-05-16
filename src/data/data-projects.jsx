fa54c81ab9531abcacc53a28f8c08f56d7d97076
haekalrfny <haekalrfny@gmail.com>
2025-05-16 13:24:55 +0700

    update wisata app

--- src/data/data-projects.jsx
+++ src/data/data-projects.jsx
@@ -85,7 +85,7 @@ export const projects = [
     desc: "This application is used to discover nearby culinary spots, specifically within Indonesia. It also includes a collection of recipes for users to explore.",
     tools: [images[6], images[7], images[10], images[15]],
     url: "https://carikuliner.vercel.app/",
-    repo: "https://github.com/haekalrfny/cari-kuliner",
+    repo: "https://github.com/haekalrfny/new_carikuliner",
   },
   {
     title: "Calculator Sederhana",
@@ -104,7 +104,7 @@ export const projects = [
     scope: "Personal",
     desc: "Useful for checking real-time weather and temperature conditions from anywhere around the world.",
     tools: [images[0], images[2], images[10]],
-    url: "https://weather-app-nu-opal.vercel.app/",
+    url: "weather-app-navy-eight.vercel.app",
     repo: "https://github.com/haekalrfny/weather-app",
   },
   {
@@ -114,8 +114,8 @@ export const projects = [
     scope: "Personal",
     desc: "This application is used to explore tourist destinations across Indonesia, complete with photos, locations, and detailed descriptions.",
     tools: [images[6], images[7], images[10]],
-    url: "https://wisata-app-psi.vercel.app/",
-    repo: "https://github.com/haekalrfny/wisata-app",
+    url: "wisata-app-five.vercel.app",
+    repo: "https://github.com/haekalrfny/new_wisata_app",
   },
   {
     title: "Kedai Matcha Banner",
